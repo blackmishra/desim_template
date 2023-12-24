490620119db9c650afe29f9602580d8b0c84cad1
blackmishra <mishra.shashank9598@gmail.com>
2023-12-24 14:40:10 +0530

    STATIC_ROOT altered

--- desim_dj_app/settings.py
+++ desim_dj_app/settings.py
@@ -128,7 +128,7 @@ if DEBUG:
 else:
     STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
 
 
