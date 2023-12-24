709cc3a6af1702e62dc69c0b7a7caafc49fc6c19
blackmishra <mishra.shashank9598@gmail.com>
2023-12-24 11:49:49 +0530

    removed poetry from build

--- build.sh
+++ build.sh
@@ -2,23 +2,11 @@
 # exit on error
 set -o errexit
 
-echo "Installing the latest version of poetry..."
-
 pip install --upgrade pip
-
 pip install djangorestframework
 # pip install redis
 pip install yagmail
 pip install python-dotenv
-pip install poetry==1.2.0
 pip install authlib
-
-rm poetry.lock
-
-poetry lock
-
-python -m poetry install
-
 python manage.py collectstatic --no-input
 python manage.py migrate
-
