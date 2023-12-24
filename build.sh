36cab1b2d4b88b676efcb463383d240f68d01a0a
blackmishra <mishra.shashank9598@gmail.com>
2023-12-24 11:33:21 +0530

    added build file

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+echo "Installing the latest version of poetry..."
+
+pip install --upgrade pip
+
+pip install djangorestframework
+# pip install redis
+pip install yagmail
+pip install python-dotenv
+pip install poetry==1.2.0
+pip install authlib
+
+rm poetry.lock
+
+poetry lock
+
+python -m poetry install
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
